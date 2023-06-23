295ec20e7f0ccd22e7be24e9c5d9a610bb9a2df5
Sead <sead@Eminas-MBP.localdomain>
2023-06-23 16:22:33 -0500

    Deploy config

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ app.use('/user', UserRouter);
 app.use('/auth', AuthRouter);
 app.use('/tournaments', TorunamentsRouter);
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Server is running on port 3000');
 });
 
